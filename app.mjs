44869cbf3d0763561d3bd6b7229ebbae3925467f
MuhammadAnas211 <manasshakil5@gmail.com>
2024-01-17 14:16:29 +0500

    counter

---
+++ app.mjs
@@ -0,0 +1,20 @@
+var count = 0;
+
+function updateCounter() {
+  document.getElementById('counter').innerText = count;
+}
+
+function increment() {
+  count++;
+  updateCounter();
+}
+
+function decrement() {
+  count--;
+  updateCounter();
+}
+
+function reset() {
+    count = 0;
+    updateCounter();
+  }
